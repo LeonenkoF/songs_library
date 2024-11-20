17361cca8124bfd65882b5ae4bd6f0a9c9e1ebb8
LeonenkoF <leonenko22222@mail.ru>
2024-11-20 21:07:48 +0500

    starting project

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port       int    `mapstructure:"PORT"`
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     int    `mapstructure:"DB_PORT"`
+	DBName     string `mapstructure:"DB_NAME"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+}
+
+func Load() (*Config, error) {
+	config := &Config{}
+
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("Can't find the file .env : ", err)
+	}
+
+	err = viper.Unmarshal(config)
+	if err != nil {
+		log.Fatal("Environment can't be loaded: ", err)
+	}
+
+	return config, nil
+}
